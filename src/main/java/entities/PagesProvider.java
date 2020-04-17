1fa9244dbf3fd5fd3e7df007c33defc1ac2a7ebd
tobolux <tobolux@gmail.com>
2020-04-17 22:40:03 +0300

    реализовано 3 сценария, объединены pageObject, подрезан PagesProvider

--- src/main/java/entities/PagesProvider.java
+++ src/main/java/entities/PagesProvider.java
@@ -2,24 +2,13 @@ package entities;
 
 import org.openqa.selenium.WebElement;
 import pages.TestPageAuthForm;
-import pages.TestPageThemes;
 
 public class PagesProvider {
     private TestPageAuthForm pageAuthForm = new TestPageAuthForm();
-    private TestPageThemes testPageThemes = new TestPageThemes();
 
-    public WebElement getElementOnPage (String nameOfPage, String nameOfElement) {
+    public WebElement getElementOnPage(String nameOfPage, String nameOfElement) {
         WebElement webElement = null;
-        switch (nameOfPage) {
-            case "Страница_авторизации":
-            case "главная_страница":
-                webElement = pageAuthForm.get(nameOfElement);
-                break;
-            case "Страница_Темы":
-                webElement = testPageThemes.get(nameOfElement);
-                break;
-            default: webElement=null;
-        }
+        webElement = pageAuthForm.get(nameOfElement);
         return webElement;
     }
 }
--- src/main/java/pages/TestPageAuthForm.java
+++ src/main/java/pages/TestPageAuthForm.java
@@ -39,7 +39,7 @@ public class TestPageAuthForm extends TestPage {
 
     @NameOfElement("Поле_ввода_логина")
     @FindBy(xpath = "//p[text()='Логин или пароль неправильны.']")
-    public WebElement authFormIncorrectCredentialsAlert ;
+    public WebElement authFormIncorrectCredentialsAlert;
 
     @FindBy(xpath = "//p[text()='Заполните оба поля.']")
     public WebElement authFormEmptyCredentialsAlert;
@@ -58,4 +58,95 @@ public class TestPageAuthForm extends TestPage {
     @NameOfElement("выход")
     @FindBy(xpath = "//button[text()='Выход']")
     public WebElement userAccountExitButton;
+
+    @NameOfElement("страница_темы")
+    @FindBy(xpath = "//a[href='/']")
+    public WebElement themePage;
+
+    @NameOfElement("кнопка_создания_новой_темы")
+    @FindBy(xpath = "//button[@class='btn btn-primary btn-block btn-outline']")
+    public WebElement themeCreate;
+
+    @NameOfElement("заголовок_темы")
+    @FindBy(xpath = "//input[@placeholder='Заголовок темы']")
+    public WebElement themeCreateTitle;
+
+    @NameOfElement("сообщение_темы")
+    @FindBy(xpath = "//textarea[@id='editor-textarea']")
+    public WebElement themeCreateDescription;
+
+    @NameOfElement("кнопка_отмены_публикации")
+    @FindBy(xpath = "//button[@class='btn btn-default btn-sm pull-right']")
+    public WebElement themeCreateCancel;
+
+    @NameOfElement("кнопка_публикации_темы")
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement themeCreateSubmit;
+
+    /**
+     * Ответить в теме
+     */
+    @NameOfElement("ответить")
+    @FindBy(xpath = "//button[@class='btn btn-primary btn-block btn-outline']")
+    public WebElement themeAnswerMessage;
+
+    @NameOfElement("тема_форума")
+    @FindBy(tagName = "h1")
+    public WebElement themeForum;
+
+    //Поиск по названию темы. Подставляется значение переменной из Feature
+//    @NameOfElement("название_темы")
+//    @FindBy(linkText = "<Заголовок темы>")
+//    public WebElement themeTitle;
+
+    @NameOfElement("название_темы")
+    @FindBy(xpath = "//div[@class='post-body']")
+    public WebElement themeName;
+
+    /*
+    //Поиск кнопки Неактивна/Активна на строке с названием темы
+    List<WebElement> element = driver.findElements(By.linkText("<Заголовок темы>"));
+    Point classname = element.get(0).getLocation();
+    int y = classname.getY() + 10; //разметка на сайте непостоянная, и кнопка в разных строках по разному съезжает
+    // относительно местонахождения названия темы, поэтому +10. Но не везде опять же работает, боремся с этим)
+    int constStar = 1185; //постоянная позиция Звездочки в кнопке по оси Y
+    Actions builder = new Actions(driver);
+    builder.moveByOffset(constStar, y).click().build().perform();
+        */
+
+//    @FindBy(xpath = "//div[@class='btn-group btn-group-justified']")
+//    public WebElement themeButtonActiveOrNotActive;
+
+    @NameOfElement("отписаться")
+    @FindBy(css = ".open > ul:nth-child(2) > li:nth-child(1) > button:nth-child(1) > span:nth-child(1)")
+    public WebElement themeButtonUnsubscribe;
+
+    @NameOfElement("подписаться")
+    @FindBy(css = ".open > ul:nth-child(2) > li:nth-child(2) > button:nth-child(1) > span:nth-child(1)")
+    public WebElement themeButtonSubscribe;
+
+    /**
+     * Вкладки
+     */
+    @NameOfElement("все")
+    @FindBy(linkText = "Все")
+    public WebElement tabAll;
+
+    @NameOfElement("мои")
+    @FindBy(xpath = "//a[@href='/my/']")
+    public WebElement tabMy;
+
+    @NameOfElement("новые")
+    @FindBy(xpath = "//a[@href='/new/']")
+    public WebElement tabNew;
+
+    @NameOfElement("непрочитанные")
+    @FindBy(xpath = "//a[@href='/unread/']")
+    public WebElement tabUnread;
+
+    @NameOfElement("подписки")
+    @FindBy(xpath = "//a[@href='/subscribed/']")
+    public WebElement tabSubscribed;
 }
+
+
