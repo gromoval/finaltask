9d3ab3a27f8a1c401f489ecc97406f39172aa578
gromoval <gromov-rabota@yandex.ru>
2020-04-18 13:20:21 +0300

    2 scenarios added

--- src/main/java/entities/PagesProvider.java
+++ src/main/java/entities/PagesProvider.java
@@ -5,20 +5,23 @@ import pages.TestPageAuthForm;
 
 public class PagesProvider {
     private TestPageAuthForm pageAuthForm = new TestPageAuthForm();
+//    private TestPageThemes testPageThemes = new TestPageThemes();
+
 
     public WebElement getElementOnPage(String nameOfPage, String nameOfElement) {
         WebElement webElement = null;
-        switch (nameOfPage) {
-            case "Страница_авторизации":
-            case "главная_страница":
-                webElement = pageAuthForm.get(nameOfElement);
-                break;
-            case "Страница_Темы":
-                webElement = testPageThemes.get(nameOfElement);
-                break;
-            default:
-                webElement = null;
-        }
+        webElement = pageAuthForm.get(nameOfElement);
+//        switch (nameOfPage) {
+//            case "Страница_авторизации":
+//            case "главная_страница":
+//                webElement = pageAuthForm.get(nameOfElement);
+//                break;
+//            case "Страница_Темы":
+//                webElement = testPageThemes.get(nameOfElement);
+//                break;
+//            default:
+//                webElement = null;
+//        }
         return webElement;
     }
 }
